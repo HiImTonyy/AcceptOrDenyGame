2bff4cecf6e7837fba80d986d433e00ac53e690f
HiImTonyy <129976747+HiImTonyy@users.noreply.github.com>
2024-11-01 18:20:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AcceptOrDenyGame
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WindowHeight = 45;
+            Console.WindowWidth = 145;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("ACCEPT OR DENY - A text-based game by Anthony Butler\n");
+            Console.ResetColor();
+
+            Console.WriteLine("1) New Game");
+            Console.WriteLine("2) Load Game");
+
+            string input = Console.ReadLine();
+        }
+    }
+}
