8d2c5556101862e68275a7e4b4c91ee62d15827f
HiImTonyy <129976747+HiImTonyy@users.noreply.github.com>
2024-11-02 15:27:58 -0400

    Added the class library files. (facepalm)

---
+++ AcceptOrDenyLibrary/Logic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptOrDenyLibrary
+{
+    public class Logic
+    {
+        public static int CreateRandomNumber(int minNumber, int maxNumber)
+        {
+            Random random = new Random();
+            int numberSelected = random.Next(minNumber, maxNumber);
+            return numberSelected;
+        }
+    }
+}
---
+++ AcceptOrDenyLibrary/Work.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptOrDenyLibrary
+{
+    public class Work
+    {
+        private int dayWage;
+        private int currentDay;
+        private int lineupCount;
+        private int bonusPay;
+        private int correctJudgements;
+        private int wrongJudgements;
+
+        public int DayWage
+        {
+            get { return dayWage; }
+            set { dayWage = value; }
+        }
+
+        public int CurrentDay
+        {
+            get { return dayWage; }
+            set { dayWage = value; }
+        }
+
+        public int LineupCount
+        {
+            get { return lineupCount; }
+            set { lineupCount = value; }
+        }
+
+        public Work()
+        {
+            dayWage = 50;
+            currentDay = 1;
+            lineupCount = 10;
+            bonusPay = 0;
+            correctJudgements = 0;
+            wrongJudgements = 0;
+        }
+
+        public static void Working()
+        {
+            Work work = new Work();
+
+            do
+            {
+                NPC npc = new NPC().GenerateNPC();
+
+                HeaderScreen(work);
+                NPC.ShowNpcID(npc);
+                Console.ReadLine();
+            } while (true);
+        }
+
+        public static void HeaderScreen(Work work)
+        {
+            DateTime localDate = DateTime.Now;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"People in line: {work.lineupCount}\tCurrent Day: {localDate.Month}/{localDate.Day}/{localDate.Year}");
+            Console.WriteLine("-----------------------------------------------\n");
+            Console.ResetColor();
+        }
+    }
+}
